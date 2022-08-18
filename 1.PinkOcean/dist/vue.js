7dc474027c351f182c1d65c24f09b8d1410deec8
Jay <1149310351@qq.com>
2022-08-18 17:55:29 +0800

    观测数据变化，递归处理对象数组

--- 1.PinkOcean/dist/vue.js
+++ 1.PinkOcean/dist/vue.js
@@ -4,10 +4,152 @@
     (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.Vue = factory());
 })(this, (function () { 'use strict';
 
+    function isFunction(val) {
+      return typeof val == 'function';
+    }
+    function isObject(val) {
+      return typeof val == 'object' && val !== null;
+    }
+    let isArray = Array.isArray;
+
+    let oldArrayPrototype = Array.prototype; // 获取数组的老的原型方法
+
+    let arrayMethods = Object.create(oldArrayPrototype); // 让arrayMethods 通过__proto__ 能获取到数组的方法
+    // arrayMethods.__proto__ == oldArrayPrototype
+    // arrayMethods.push = function
+
+    let methods = [// 只有这七个方法 可以导致数组发生变化
+    'push', 'shift', 'pop', 'unshift', 'reverse', 'sort', 'splice'];
+    methods.forEach(method => {
+      arrayMethods[method] = function (...args) {
+        // 数组新增的属性 要看一下是不是对象，如果是对象 继续进行劫持
+        // 需要调用数组原生逻辑
+        oldArrayPrototype[method].call(this, ...args); // todo... 可以添加自己逻辑 函数劫持 切片
+
+        let inserted = null;
+        let ob = this.__ob__;
+
+        switch (method) {
+          case 'splice':
+            // 修改 删除  添加  arr.splice(0,0,100,200,300)
+            inserted = args.slice(2); // splice方法从第三个参数起 是增添的新数据
+
+            break;
+
+          case 'push':
+          case 'unshift':
+            inserted = args; // 调用push 和 unshift 传递的参数就是新增的逻辑
+
+            break;
+        } // inserted[] 遍历数组 看一下它是否需要进行劫持
+
+
+        if (inserted) ob.observeArray(inserted);
+      };
+    }); // 属性的查找：是先找自己身上的，找不到去原型上查找
+
+    // 2.每个原型上都有一个constructor属性 指向 函数本身 Function.prototype.constrcutr = Function
+
+    class Observer {
+      constructor(value) {
+        // 不让__ob__ 被遍历到
+        // value.__ob__ = this; // 我给对象和数组添加一个自定义属性
+        Object.defineProperty(value, '__ob__', {
+          value: this,
+          enumerable: false // 标识这个属性不能被列举出来，不能被循环到
+
+        });
+
+        if (isArray(value)) {
+          // 更改数组原型方法, 如果是数组 我就改写数组的原型链
+          value.__proto__ = arrayMethods; // 重写数组的方法
+
+          this.observeArray(value);
+        } else {
+          this.walk(value); // 核心就是循环对象
+        }
+      }
+
+      observeArray(data) {
+        // 递归遍历数组，对数组内部的对象再次重写 [[]]  [{}]
+        // vm.arr[0].a = 100;
+        // vm.arr[0] = 100;
+        data.forEach(item => observe(item)); // 数组里面如果是引用类型那么是响应式的
+      }
+
+      walk(data) {
+        Object.keys(data).forEach(key => {
+          // 要使用defineProperty重新定义
+          defineReactive(data, key, data[key]);
+        });
+      }
+
+    } // vue2 应用了defineProperty需要一加载的时候 就进行递归操作，所以好性能，如果层次过深也会浪费性能
+    // 1.性能优化的原则：
+    // 1) 不要把所有的数据都放在data中，因为所有的数据都会增加get和set
+    // 2) 不要写数据的时候 层次过深， 尽量扁平化数据 
+    // 3) 不要频繁获取数据
+    // 4) 如果数据不需要响应式 可以使用Object.freeze 冻结属性 
+
+
+    function defineReactive(obj, key, value) {
+      // vue2 慢的原因 主要在这个方法中
+      observe(value); // 递归进行观测数据，不管有多少层 我都进行defineProperty
+
+      Object.defineProperty(obj, key, {
+        get() {
+          // 后续会有很多逻辑
+          return value; // 闭包，次此value 会像上层的value进行查找
+        },
+
+        set(newValue) {
+          // 如果设置的是一个对象那么会再次进行劫持
+          if (newValue === value) return;
+          observe(newValue);
+          console.log('修改');
+          value = newValue;
+        }
+
+      });
+    }
+
+    function observe(value) {
+      // 1.如果value不是对象，那么就不用观测了，说明写的有问题
+      if (!isObject(value)) {
+        return;
+      }
+
+      if (value.__ob__) {
+        return; // 一个对象不需要重新被观测
+      } // 需要对对象进行观测 （最外层必须是一个{} 不能是数组）
+      // 如果一个数据已经被观测过了 ，就不要在进行观测了， 用类来实现，我观测过就增加一个标识 说明观测过了，在观测的时候 可以先检测是否观测过，如果观测过了就跳过检测
+
+
+      return new Observer(value);
+    }
+
+    function initState(vm) {
+      const opts = vm.$options;
+
+      if (opts.data) {
+        initData(vm);
+      }
+    }
+
+    function initData(vm) {
+      let data = vm.$options.data; // 用户传入的数据
+
+      data = vm._data = isFunction(data) ? data.call(vm) : data; // _data 已经是响应式的了
+      // 需要将data变成响应式的 Object.defineProperty， 重写data中的所有属性
+
+      observe(data); // 观测对象中的属性
+    }
+
     function initMixin(Vue) {
       Vue.prototype._init = function (options) {
         const vm = this;
         vm.$options = options;
+        initState(vm);
 
         if (vm.$options.el) ;
       };
--- 1.PinkOcean/src/state.js
+++ 1.PinkOcean/src/state.js
@@ -1,3 +1,4 @@
+import { observe } from "./observe";
 import { isFunction } from "./utils";
 
 export function initState(vm){
@@ -13,4 +14,6 @@ function initData(vm){
     
     data = vm._data =  isFunction(data) ? data.call(vm) : data; // _data 已经是响应式的了
 
+    // 需要将data变成响应式的 Object.defineProperty， 重写data中的所有属性
+    observe(data); // 观测对象中的属性
 }
